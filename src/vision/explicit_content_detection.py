a45cb3c10a3eb7b09029eb65d84f4114e421885e
antamb <anta.aidara@gmail.com>
2017-07-01 21:15:10 +0200

    Update explicit content detection

--- src/vision/explicit_content_detection.py
+++ src/vision/explicit_content_detection.py
@@ -1,5 +1,5 @@
 from vision.vision_helpers import VisionHelper
-
+from constants import Constants
 
 class ExplicitContentDetection:
 
@@ -10,15 +10,15 @@ class ExplicitContentDetection:
         image = self._vision.get_vision_image()
         explicit_content = image.detect_safe_search()
 
-        from constants import Constants
+        print("ADULT: {}\n MEDICAL: {}\n VIOLENCE: {}\n ".format(explicit_content.adult,
+                                                                 explicit_content.medical,
+                                                                 explicit_content.violence))
+
         if explicit_content.adult.name in Constants.RATINGS:
-            print(explicit_content.adult.name)
             response = "I see some adult content, please, make sure that there is no under-age people in the room"
         elif explicit_content.medical.name in Constants.RATINGS:
-            print(explicit_content.medical.name)
             response = "I see medical stuff"
         elif explicit_content.violence.name in Constants.RATINGS:
-            print(explicit_content.violence.name)
             response = "What I see is violent, do you need me to call 911 ?"
         else:
             response = "I see nothing special"
