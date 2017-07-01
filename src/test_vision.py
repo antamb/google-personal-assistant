7a10e264628c89152268f3f35e3d32fb5bd281dc
antamb <anta.aidara@gmail.com>
2017-07-01 11:16:43 +0200

    Modify test vision script

--- src/test_vision.py
+++ src/test_vision.py
@@ -1,6 +1,6 @@
 from vision.face_detection import FaceDetection
 
-face_detection = FaceDetection
-face_detection.detect_faces()
+face_detection = FaceDetection()
+FaceDetection.detect_faces(face_detection)
 
 
