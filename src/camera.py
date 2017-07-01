d3eecdb74eea4cf9106f5305fbca27b81dcbd8d1
antamb <anta.aidara@gmail.com>
2017-07-01 12:24:14 +0200

    Modify camera utils

--- src/camera.py
+++ src/camera.py
@@ -1,21 +1,24 @@
 import os
 import os.path
-import picamera
+from picamera import PiCamera
 
 from time import sleep
 
 
 class CameraUtils:
-    
+
     IMAGE_DIR = "/Pictures/screenshots/"
 
     @staticmethod
     def take_screenshot():
         file = os.path.join(CameraUtils.IMAGE_DIR, "screenshot.jpg")
-        with picamera.PiCamera as camera:
+        camera = PiCamera()
+        try:
             print("Give me your best smile")
             camera.start_preview()
             sleep(2)
             camera.capture(file)
             camera.stop_preview()
+        finally:
             print("Ok now you can relax")
+            camera.close()
